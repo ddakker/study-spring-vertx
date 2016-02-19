395c19fc0d0bd8e5dbf9185b6864238272910f74
ddakker <ddakker@naver.com>
2016-02-19 19:21:58 +0900

    .

--- src/main/java/pe/kr/ddakker/study/Server.java
+++ src/main/java/pe/kr/ddakker/study/Server.java
@@ -8,6 +8,7 @@ import io.vertx.core.Vertx;
 import pe.kr.ddakker.study.config.AppContext;
 import pe.kr.ddakker.study.verticle.DelayExecVerticle;
 import pe.kr.ddakker.study.verticle.ServerVerticle;
+import pe.kr.ddakker.study.verticle.ServerVerticle2;
 import pe.kr.ddakker.study.verticle.SockjsVerticle;
 
 public class Server {
@@ -18,6 +19,7 @@ public class Server {
 		//vertx.deployVerticle(new DelayExecVerticle(), new DeploymentOptions().setWorker(true).setInstances(10));
 		vertx.deployVerticle(DelayExecVerticle.class.getName(), new DeploymentOptions().setWorker(true).setInstances(10));
 		vertx.deployVerticle(new ServerVerticle());
+		vertx.deployVerticle(new ServerVerticle2());
 		vertx.deployVerticle(new SockjsVerticle());
 	}
 }
---
+++ src/main/java/pe/kr/ddakker/study/verticle/ServerVerticle2.java
@@ -0,0 +1,43 @@
+package pe.kr.ddakker.study.verticle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.parsetools.RecordParser;
+
+public class ServerVerticle2 extends AbstractVerticle {
+	private static Logger log = LoggerFactory.getLogger(ServerVerticle2.class);
+	
+	@Override
+	public void start() throws Exception {
+		vertx.createNetServer().connectHandler(socket -> {
+
+			String handlerID = socket.writeHandlerID();
+
+			socket.handler(RecordParser.newDelimited("\n", buffer -> {
+
+				String message = buffer.toString().trim();
+
+				log.debug(socket.remoteAddress() + " (" + handlerID + ") => " + message + "\r\n");
+
+
+				/*vertx.eventBus().<String> send(DelayExecVerticle.ALL_PRODUCTS_ADDRESS, message, result -> {
+					if (result.succeeded()) {
+						log.debug("SS result.result().body(): " + result.result().body());
+						socket.write("Receive => " + message + "\r\n");
+					} else {
+						log.error("FF result.cause().toString(): " + result.cause().toString());
+					}
+				});*/
+				
+				//vertx.eventBus().<String> send(DelayExecVerticle.ALL_PRODUCTS_ADDRESS, message);
+				
+				//vertx.eventBus().<String> send("chat.to.server", ": 테스트여 00");
+				vertx.eventBus().send("chat.to.server", message);
+
+			}));
+
+		}).listen(9998);
+	}
+}
